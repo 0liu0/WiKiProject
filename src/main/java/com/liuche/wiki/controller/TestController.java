60f45b7b9cb379a10473f2c5be22c6795152a83f
liuche <706716852@qq.com>
2023-03-01 15:21:17 +0800

    2-8 增加了自定义配置项的使用

--- src/main/java/com/liuche/wiki/controller/TestController.java
+++ src/main/java/com/liuche/wiki/controller/TestController.java
@@ -1,14 +1,16 @@
 package com.liuche.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 这个注解点进去有ResponseBody注解，其作用是返回给浏览器json格式的内容。@Controller是返回一个页面，这个项目中用不到
 @RequestMapping("/test")
 public class TestController {
-
+    @Value("${test.hello:shuaishuai}") // :后面的值是默认值，如果配置文件中没有提供值就会配置成默认值
+    private String hello;
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + hello;
     }
 }
