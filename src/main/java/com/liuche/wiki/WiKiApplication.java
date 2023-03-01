0336c671d8d409a2743491df33e8a4644b86e2c9
liuche <706716852@qq.com>
2023-03-01 14:03:20 +0800

    项目初始化

---
+++ src/main/java/com/liuche/wiki/WiKiApplication.java
@@ -0,0 +1,13 @@
+package com.liuche.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiKiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiKiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuche/wiki/WiKiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuche.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiKiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
