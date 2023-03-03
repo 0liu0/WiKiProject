44a7fab4efb35f851a5c6556b13048c3880d5e84
liuche <706716852@qq.com>
2023-03-03 23:00:44 +0800

    5-1 集成PageHelper插件 将mapper日志级别改为打印mapper执行sql语句的所有日志

--- src/main/java/com/liuche/wiki/service/impl/EbookServiceImpl.java
+++ src/main/java/com/liuche/wiki/service/impl/EbookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.liuche.wiki.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.liuche.wiki.domain.Ebook;
 import com.liuche.wiki.mapper.EbookMapper;
 import com.liuche.wiki.req.EbookReq;
@@ -28,9 +30,17 @@ public class EbookServiceImpl implements EbookService {
         if (!ObjectUtils.isEmpty(name)){
             name = "%" + name + "%";
             req.setName(name);
+            PageHelper.startPage(1,3);
             ebooks = ebookMapper.selectAll(req);
+            PageInfo<Ebook> p = new PageInfo<>();
+            System.out.println(p.getPages());
+            System.out.println(p.getTotal());
         }else {
+            PageHelper.startPage(1,3);
             ebooks = ebookMapper.selectAll1();
+            PageInfo<Ebook> p = new PageInfo<>();
+            System.out.println(p.getPages());
+            System.out.println(p.getTotal());
         }
         return CopyUtil.copyList(ebooks, EbookResp.class);
     }
