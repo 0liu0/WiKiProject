e2daa21fcf8dc60fe8d7edda0d864826fa2ccc55
liuche <706716852@qq.com>
2023-03-03 18:42:35 +0800

    4-5 增加AOP，打印接口耗时，请求参数和返回参数，并且去点过滤器和拦截器

---
+++ src/main/java/com/liuche/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package com.liuche.wiki.config;
+
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer { // 增加拦截器
+//
+//    @Resource
+//    private LogInterceptor logInterceptor;
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+//    }
+//}
