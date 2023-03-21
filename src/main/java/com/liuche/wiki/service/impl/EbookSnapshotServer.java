377889b5dbb4a38fac1af8e6cb9e1c25c1be9985
liuche <lzc706716852@gmail.com>
2023-03-21 19:59:36 +0800

    10-4 解决了前后端雪花算法不一致的问题

--- src/main/java/com/liuche/wiki/service/impl/EbookSnapshotServer.java
+++ src/main/java/com/liuche/wiki/service/impl/EbookSnapshotServer.java
@@ -1,9 +1,7 @@
 package com.liuche.wiki.service.impl;
 
-import com.liuche.wiki.domain.EbookSnapshot;
 import com.liuche.wiki.mapper.EbookSnapshotMapper;
 import com.liuche.wiki.resp.StatisticResp;
-import com.liuche.wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +16,12 @@ public class EbookSnapshotServer {
         // 更新ebook_snapshot这张表格
         ebookSnapshotMapper.genEbookSnapshot();
     }
-
     /**
      * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
      */
     public List<StatisticResp> getStatistic() {
         return ebookSnapshotMapper.getStatistic();
     }
-
     /**
      * 30天数值统计
      */
